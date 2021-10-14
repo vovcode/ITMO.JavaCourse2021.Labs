8893e0e42411d591e665ac0ab08017cfc5d7c9c2
vovcode <cromwell614@gmail.com>
2021-10-14 15:57:32 +0300

    refactoring

--- src/Labs/Lab01/Lab01.java
+++ src/Labs/Lab01/Lab01.java
@@ -5,55 +5,59 @@ import java.util.Scanner;
 public class Lab01 {
 
     public static void main(String[] args) {
-        saySomeStuff1();
+        saySomeStuff();
+        solveMyMath();
         solveMyMath2();
-        solveMyMath3();
-        multiplyNums4();
-        getIntsFromInput5();
-        numOddOrEven6();
+        multiplyNums();
+        getIntsFromInput();
+        numOddOrEven();
     }
-    public static void saySomeStuff1(){
+
+    public static void saySomeStuff() {
         System.out.println("Я");
         System.out.println("хорошо");
         System.out.println("знаю");
         System.out.println("Java");
     }
-    public static void solveMyMath2(){
-        System.out.println(( 46 + 10 ) * ( 10 / 3 ));
-        System.out.println((29 ) * ( 4 ) * (- 15 ));
+
+    public static void solveMyMath() {
+        System.out.println((46 + 10) * (10 / 3));
+        System.out.println((29) * (4) * (-15));
     }
-    public static void solveMyMath3(){
+
+    public static void solveMyMath2() {
         int number = 10500;
-        int result = (number/10)/10;
+        int result = (number / 10) / 10;
         System.out.println(result);
     }
-    public static void multiplyNums4(){
+
+    public static void multiplyNums() {
         double num1 = 3.6;
         double num2 = 4.1;
         double num3 = 5.9;
         double result = num1 * num2 * num3;
         System.out.println(result);
     }
-    public static void getIntsFromInput5(){
+
+    public static void getIntsFromInput() {
         Scanner in = new Scanner(System.in);
-        int num1=in.nextInt();
-        int num2=in.nextInt();
-        int num3=in.nextInt();
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int num3 = in.nextInt();
 
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(num3);
     }
-    public static void numOddOrEven6(){
+
+    public static void numOddOrEven() {
         Scanner in = new Scanner(System.in);
-        int b =in.nextInt();
-        if (b % 2 == 0 & b > 100){
+        int b = in.nextInt();
+        if (b % 2 == 0 & b > 100) {
             System.out.print("Выход за пределы диапазона");
-        }
-        else if (!(b % 2 == 0)){
+        } else if (!(b % 2 == 0)) {
             System.out.print("Нечетное");
-        }
-        else if (b % 2 == 0){
+        } else if (b % 2 == 0) {
             System.out.println("Четное");
         }
     }
--- src/Labs/Lab02/Lab02.java
+++ src/Labs/Lab02/Lab02.java
@@ -5,15 +5,21 @@ public class Lab02 {
         writeOddNums1();
         //divByThreeFiveBoth2();
     }
-    public static  void writeOddNums1(){
-        for (int i = 0; i <=99 ; i++) {
-            if (i % 2 != 0){
+
+    public static void writeOddNums1() {
+        for (int i = 0; i <= 99; i++) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
-    //public static void divByThreeFiveBoth2(){
-       // for (int i = 0; i <= 100 ; i++) {
-          //  if
+
+    public static void divByThreeFiveBoth2() {
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 15 == 0) {
+
+            }
         }
-  //  }
+    }
+}
